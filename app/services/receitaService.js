9b1c756d1411fd298de58687b275ecf8758da755
gabriel-batistanascimento <gabriel.batista.nascimento96@gmail.com>
2025-11-05 19:16:08 -0300

    Update receitas with new ingredients and instructions

--- app/services/receitaService.js
+++ app/services/receitaService.js
@@ -1,11 +1,12 @@
 import ReceitaEntity from "../entities/receitaEntity";
 
 let receitas = [
-  new ReceitaEntity(1, "Bolo de Cenoura", ["cenoura", "farinha", "açúcar"], "Misture tudo e asse."),
-  new ReceitaEntity(2, "Arroz", ["arroz", "água", "sal"], "Cozinhe o arroz até ficar macio."),
+  new ReceitaEntity(1, "Bolo de Cenoura", ["cenoura", "farinha", "açúcar", "oléo", "ovo", "fermento"], "Bata a cenoura descascada com os ovos, açúcar e oléo, ate ficar homogênea. Em seguida, misture com a farinha e por último acrescente o fermento. Disponha em uma assadeira previamente untadada e asse por 40 min à 180°."),
+  new ReceitaEntity(2, "Arroz", ["arroz", "água", "sal", "cebola", "alho"], "Refogue a cebola e o alho ate ficarem transparente. Após isso colocar o arroz, o sal e completar com água, assim que secar, prove o arroz e veja se precisa de mais sal e/ou cozinhar mais."),
   new ReceitaEntity(3, "Omelete Simples", ["ovo", "sal", "pimenta"], "Bata os ovos, tempere e frite."),
-  new ReceitaEntity(4, "Salada Rápida", ["alface", "tomate", "azeite", "sal"], "Misture todos os ingredientes e tempere."),
-  new ReceitaEntity(5, "Panqueca Doce", ["farinha", "ovo", "leite", "açúcar"], "Misture os ingredientes e frite em frigideira."),
+  new ReceitaEntity(4, "Salada Rápida", ["alface", "tomate", "rucúla", "azeite", "sal", "limão", "pimenta"], "Pique o tomate, rasgue as folhas em pedaços. Misture o azeite, limão, sal e pimenta. Misture todos os ingredientes e regue o molho por cima."),
+  new ReceitaEntity(5, "Carbonara", ["ovo", "bacon", "parmesão", "macarrão", "sal", "pimenta"], "Pique o bacon, separe as claras das gemas e rale o parmesão. Frite o bacon e reserve, em uma tigela misture as gemas e o queijo ralado. Cozinhe o macarrão em uma panela a parte com água fervente, retire quando estiver al dente. Na panela que fez o bacon, coloque o macarrão com um pouco da água, a mistura de gemas. Misture delicadamente para encorporar os ingredientes. Por fim, coloque o bacon frito e sirva.")
+  new ReceitaEntity(6, "Panqueca Doce", ["farinha", "ovo", "leite", "açúcar", "fermento"], "Bata todos os ingredientes em um liquidificador e frite em uma frigideira quente. Sirva com uma calda doce ou sorvete."),
 ];
 
 export default {
@@ -29,3 +30,4 @@ export default {
     return Promise.resolve(receita);
   },
 };
+
