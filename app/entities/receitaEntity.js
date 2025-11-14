7fd86c5967619a8e1eb136372a1672ff3cebbb54
Emanuel Tarso <143852548+EmanuelTarso@users.noreply.github.com>
2025-11-13 22:02:38 -0300

    Update receitaEntity.js
    Adicionado o campo "imagem"

--- app/entities/receitaEntity.js
+++ app/entities/receitaEntity.js
@@ -1,8 +1,9 @@
 export default class ReceitaEntity {
-  constructor(id, nome, ingredientes, modoPreparo) {
+  constructor(id, nome, ingredientes, modoPreparo, imagem) {
     this.id = id;
     this.nome = nome;
     this.ingredientes = ingredientes;
     this.modoPreparo = modoPreparo;
+    this.imagem = imagem;
   }
 }
