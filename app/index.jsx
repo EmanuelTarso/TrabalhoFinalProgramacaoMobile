9c2e44aeabde56da1bca59cc70b7f214ca570e7a
Emanuel Tarso <143852548+EmanuelTarso@users.noreply.github.com>
2025-11-14 00:17:16 -0300

    Update index.jsx
    estilização

--- app/index.jsx
+++ app/index.jsx
@@ -6,14 +6,18 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-       <Text style={styles.title}>Rango Certo</Text>
+      
+      <Text style={styles.title}>Rango Certo</Text>
+
+      <View style={styles.imagemEspaco} />
+
       <Text style={styles.subtitle}>
-        Explore receitas e adicione as suas receitas preferidas.
+        Registre, explore e desfrute do seu cardápio pessoal!
       </Text>
 
       <Pressable
         style={styles.botao}
-        onPress={() => router .push("/view/receitaListView")}
+        onPress={() => router.push("/view/receitaListView")}
       >
         <Text style={styles.texto}>Ver Receitas</Text>
       </Pressable>
@@ -35,14 +39,41 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 20,
     gap: 20,
+    backgroundColor: "#FFF8F0", 
   },
-  title: { fontSize: 28, fontWeight: "700" },
-  subtitle: { fontSize: 16, textAlign: "center", marginBottom: 20 },
+  imagemEspaco: {
+    width: "50%",
+    height: 200,
+    borderRadius: 12,
+    backgroundColor: "#f0f0f0",
+    borderWidth: 1,
+    borderColor: "#ddd",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2,
+    marginBottom: 20,
+  },
+  title: { 
+    fontSize: 28, 
+    fontWeight: "700", 
+    color: "#000",
+    textAlign: "center",
+  },
+ subtitle: {
+  fontSize: 16,          
+  fontWeight: "600",     
+  color: "#333",     
+  textAlign: "center",    
+  marginBottom: 15,     
+  fontStyle: "italic",   
+},
   botao: {
     backgroundColor: "#Ffa500",
     paddingVertical: 12,
     paddingHorizontal: 30,
     borderRadius: 10,
   },
-  texto: { color: "#fff", fontSize: 18 },
+  texto: { color: "#fff", fontSize: 18, fontWeight: "bold" },
 });
