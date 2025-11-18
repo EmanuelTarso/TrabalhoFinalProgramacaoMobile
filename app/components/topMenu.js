421da470efbb9f783c37d7439e45e32b1fdca6cb
letbeth <leticiabethonicosf@gmail.com>
2025-11-18 00:16:35 -0300

    Update and rename navigation to topMenu.js

---
+++ app/components/topMenu.js
@@ -0,0 +1,25 @@
+mport { useRouter } from "expo-router";
+<Pressable
+            style={styles.itemBox}
+            onPress={() => {
+              router.push("../view/receitaListView.jsx");
+            }}
+          >
+            <Text style={styles.item}> Lista de Receitas</Text>
+          </Pressable>
+<Pressable
+            style={styles.itemBox}
+            onPress={() => {
+              router.push("../view/receitaFormView.jsx");
+            }}
+          >
+            <Text style={styles.item}> Formulário </Text>
+          </Pressable>
+<Pressable
+            style={styles.itemBox}
+            onPress={() => {
+              router.push("../app/index.jsx");
+            }}
+          >
+            <Text style={styles.item}> Index </Text>
+          </Pressable>
