d9c97cb2c42a427db765f8a2ff27883f0d423707
Emanuel Tarso <143852548+EmanuelTarso@users.noreply.github.com>
2025-11-18 21:11:05 -0300

    Update receitaEntity.js
    add favoritos

--- app/entities/receitaEntity.js
+++ app/entities/receitaEntity.js
@@ -1,9 +1,10 @@
 export default class ReceitaEntity {
-  constructor(id, nome, ingredientes, modoPreparo, imagem) {
+  constructor(id, nome, ingredientes, modoPreparo, imagem, favorito = false) {
     this.id = id;
     this.nome = nome;
     this.ingredientes = ingredientes;
     this.modoPreparo = modoPreparo;
     this.imagem = imagem;
+    this.favorito = favorito;
   }
 }
